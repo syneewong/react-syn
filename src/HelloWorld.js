ee0109dd0a978006c8e5bccb7989d7bf8d260820
Syn-Ee Wong <synee.wong@ie.com.au>
2017-09-01 13:04:46 +1000

    Introducing props https://medium.com/@diamondgfx/learning-react-with-create-react-app-part-2-3ad99f38b48d.

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './HelloWorld.css';
 
-const HelloWorld = () => {
+const HelloWorld = (props) => {
 	return (
-		<div className="HelloWorld">Hello World!</div>
+		<div className="HelloWorld">Hello {props.name}!</div>
 	);
 };
 
