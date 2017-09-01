7c0e802de209c94a2f5a7e6e58ce6769d8890788
Syn-Ee Wong <synee.wong@ie.com.au>
2017-09-01 15:05:38 +1000

    Map out each array into a new array with an anonymous function. Assign a key to make it unique.

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -3,11 +3,26 @@ import HelloWorld from './HelloWorld';
 import './HelloWorldList.css';
 
 class HelloWorldList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			greetings : ['Syn-Ee', 'Eeshenn', 'Bender']
+		};
+	}
+
+	// map operation, which says “loop over the array and call a function for each element in that array,
+	// storing the results in a new array”.
+	renderGreetings() {
+		return this.state.greetings.map(name => ( // map each item in the array to a special anonymous function
+    		// key helps make it unique
+    		<HelloWorld key={name} name={name}/>
+  		));
+	}
+
 	render() {
 		return (
 	      	<div className="HelloWorldList">
-	        	<HelloWorld name="Syn-Ee"/>
-	        	<HelloWorld name="Eeshenn"/>
+	        	{this.renderGreetings()}
 	      	</div>
 		);
 	}
